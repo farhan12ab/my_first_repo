f120712c3cf5cec9587d97445353dbbed7edacf7
farhan12ab <farhanadekunle23@gmail.com>
2025-01-24 22:38:54 +0100

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -5,16 +5,16 @@ using namespace std;
 int main(){
   int x{};
   int y{};
-  int sum{};
-  sum = x+y;
-  int diff{};
-  diff = x-y;
+//   int sum{};
+//   sum = x+y;
+//   int diff{};
+//   diff = x-y;
   cout << "x : " ;
   cin >> x;
   cout << "y : " ;
   cin >> y;
-  cout << "The sum of " << x << " and " << y << " is " << sum << endl;
-  cout << "The difference between " << x << " and " << y << " is " << diff << endl;
+  cout << "The sum of " << x << " and " << y << " is " << x + y << endl;
+  cout << "The difference between " << x << " and " << y << " is " << x - y << endl;
 
   return 0;
 }
